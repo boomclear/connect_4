55a903fe1d32c41583dd053ebefedd19eaaa065f
Jeff Nelson <jpnelson85@gmail.com>
2023-04-06 19:40:02 -0500

    add player turn spec test

---
+++ spec/player_turn_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'spec_helper'
+
+RSpec.describe PlayerTurn do
+  it 'exists' do
+    player_turn = PlayerTurn.new
+    expect(player_turn).to be_a(PlayerTurn)
+  end
+
+  
+end
