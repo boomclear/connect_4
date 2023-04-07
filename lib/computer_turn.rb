9fe364dd6cc3651b81822938f87c32b43082f677
Austin Woods <austinwoods99@gmail.com>
2023-04-07 10:27:03 -0400

    Complete computer_makes_move method

--- lib/computer_turn.rb
+++ lib/computer_turn.rb
@@ -6,36 +6,102 @@ class ComputerTurn
   end
 
   def computer_makes_move(board)
-    puts "Select the column to drop your coin"
+    puts "Computer Turn"
     location_array = board.columns.keys
     location = location_array.sample
-
+    array_pos = 5
+    
     if location == "A"
-
+      until board.cells[array_pos][0].value == "*" && array_pos > -1 do 
+        array_pos -= 1
+      end
       
-    end
-      start from bottom...check if "*"
-      if not *, move up one index position
-        check for * again, if not *, move up one index position
-        if move up 6, then "Choose another column"
-
-    if "B"
-      repeat
-          
-      # until board.cell.value == "*" then place piece
-      # else move up one...loop 
-      #   start with bottom index position
-
-
+      if array_pos > -1
+        board.cells[array_pos][0].value = "O"
+        board.render_board
+        board.print_board
+      else
+        computer_makes_move(board)
       end
-      bottom_to_top = -1
-      board.columns["A"][bottom_to_top] << board.cell.value
-      bottom_to_top += @columns["A"].count
-      if bottom_to_top > 5 
-        return puts "Choose another column!"
+
+   elsif location == "B"
+      until board.cells[array_pos][1].value == "*" && array_pos > -1 do 
+        array_pos -= 1
       end
-      @cells[5 - bottom_to_top][0].value = columns["A"].last
-      render_board
-      print_board
+      
+      if array_pos > -1
+        board.cells[array_pos][1].value = "O"
+        board.render_board
+        board.print_board
+      else
+        computer_makes_move(board)
+      end
+
+    elsif location == "C"
+      until board.cells[array_pos][2].value == "*" && array_pos > -1 do 
+        array_pos -= 1
+      end
+      
+      if array_pos > -1
+        board.cells[array_pos][2].value = "O"
+        board.render_board
+        board.print_board
+      else
+        computer_makes_move(board)
+      end
+
+    elsif location == "D"
+      until board.cells[array_pos][3].value == "*" && array_pos > -1 do 
+        array_pos -= 1
+      end
+      
+      if array_pos > -1
+        board.cells[array_pos][3].value = "O"
+        board.render_board
+        board.print_board
+      else
+        computer_makes_move(board)
+      end
+
+    elsif location == "E"
+      until board.cells[array_pos][4].value == "*" && array_pos > -1 do 
+        array_pos -= 1
+      end
+      
+      if array_pos > -1
+        board.cells[array_pos][4].value = "O"
+        board.render_board
+        board.print_board
+      else
+        computer_makes_move(board)
+      end
+
+    elsif location == "F"
+      until board.cells[array_pos][5].value == "*" && array_pos > -1 do 
+        array_pos -= 1
+      end
+      
+      if array_pos > -1
+        board.cells[array_pos][5].value = "O"
+        board.render_board
+        board.print_board
+      else
+        computer_makes_move(board)
+      end
+
+    elsif location == "G"
+      until board.cells[array_pos][6].value == "*" && array_pos > -1 do 
+        array_pos -= 1
+      end
+      
+      if array_pos > -1
+        board.cells[array_pos][6].value = "O"
+        board.render_board
+        board.print_board
+      else
+        computer_makes_move(board)
+      end
+
     end
+  end
 end
