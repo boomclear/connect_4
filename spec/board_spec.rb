f79b6cffd1ed9baf38a423115eac4811aa326470
Jeff Nelson <jpnelson85@gmail.com>
2023-04-10 15:44:56 -0500

    update board spec file

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,20 +3,17 @@ require 'spec_helper'
 RSpec.describe Board do 
   it "exists" do 
     board = Board.new
-
     expect(board).to be_a Board
   end
 
   it "has attributes" do
     board = Board.new
-
     expect(board.cells).to be_a(Array)
     expect(board.columns).to be_a(Hash)
   end
 
   it 'can render board' do
     board = Board.new
-
     expect(board.render_board).to be_a Array
   end
 
