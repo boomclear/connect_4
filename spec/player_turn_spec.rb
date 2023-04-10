5fbd6be27e9281dd6179e1fa8a629b0f0841014d
Jeff Nelson <jpnelson85@gmail.com>
2023-04-10 18:11:11 -0500

    update player turn spec tests

--- spec/player_turn_spec.rb
+++ spec/player_turn_spec.rb
@@ -6,9 +6,19 @@ RSpec.describe PlayerTurn do
     expect(player).to be_a(PlayerTurn)
   end
 
+  it 'attributes' do
+    player = PlayerTurn.new
+    expect(player.array_pos).to eq(5)
+    expect(player.location).to eq("")
+  end
   it 'player makes move method' do
     player = PlayerTurn.new
     board = Board.new
+    @location = "A"
+    
     expect(player.player_makes_move(board)).to eq(nil)
+    expect(board.cells[]).to eq
   end
+
+
 end
