798c66ce874456ca83df49d543f65ac72b98ba23
Jeff Nelson <jpnelson85@gmail.com>
2023-04-11 08:59:28 -0500

    add main menu method test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,18 +8,30 @@ RSpec.describe Game do
   end
 
   it 'has attributes' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.board).to eq(board)
+    board_1 = Board.new
+    game = Game.new(board_1)
+    expect(game.board).to eq(board_1)
     expect(game.player).to be_a(PlayerTurn)
     expect(game.computer).to be_a(ComputerTurn)
     expect(game.player_turn).to eq(0)
     expect(game.computer_turn).to eq(0)
   end
 
-  it 'main_menu method' do
+  it 'main_menu method with P' do
     board = Board.new
     game = Game.new(board)
-    expect(board.main_menu).to eq("Welcome to Connect 4" "Press P to play and Q to quit")
+    allow($stdin).to receive(:gets).and_return("P\n") 
+    expect(game).to receive(:start)
+    game.main_menu
   end
+  
+  it 'main_menu method with Q' do
+    board = Board.new
+    game = Game.new(board)
+    allow($stdin).to receive(:gets).and_return("Q\n") 
+    expect(game).to receive(:main_menu)
+    game.main_menu
+  end
+
+  
 end
