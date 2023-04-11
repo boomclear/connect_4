e57c06a21ba6af627c469cb59fef2eec7610bb25
Jeff Nelson <jpnelson85@gmail.com>
2023-04-11 08:38:32 -0500

    update game spec file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe Game do
     expect(game).to be_a(Game)
   end
 
-  
+  it 'has attributes' do
+    board = Board.new
+    game = Game.new(board)
+    expect(game.board).to eq(board)
+    expect(game.player).to be_a(PlayerTurn)
+    expect(game.computer).to be_a(ComputerTurn)
+    expect(game.player_turn).to eq(0)
+    expect(game.computer_turn).to eq(0)
+  end
+
+  it 'main_menu method' do
+    board = Board.new
+    game = Game.new(board)
+    expect(board.main_menu).to eq("Welcome to Connect 4" "Press P to play and Q to quit")
+  end
 end
