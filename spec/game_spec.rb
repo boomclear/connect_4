12698de6378b095a2fd031602ba7de7cd6998bf5
Jeff Nelson <jpnelson85@gmail.com>
2023-04-13 10:03:02 -0500

    update game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -33,42 +33,6 @@ RSpec.describe Game do
     game.main_menu
   end
 
-  it 'computer_win methods diagonal right' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.computer_win?).to eq(false)
-    game.computer_win_diagonal_right
-    expect(game.computer_win?).to eq(true)
-    expect(game.computer_win).to output("You lost! :(\n").to_stdout
-  end
-
-  it 'computer_win methods diagonal left' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.computer_win?).to eq(false)
-    game.computer_win_diagonal_left
-    expect(game.computer_win?).to eq(true)
-    expect(game.computer_win).to output("You lost! :(\n").to_stdout
-  end
-
-  it 'player_win methods diagonal left' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.player_win?).to eq(false)
-    game.player_win_diagonal_left
-    expect(game.player_win?).to eq(true)
-    expect(game.player_win).to output("You won!\n").to_stdout
-  end
-
-  it 'player_win methods diagonal right' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.player_win?).to eq(false)
-    game.player_win_diagonal_right
-    expect(game.player_win?).to eq(true)
-    expect(game.player_win).to output("You won!\n").to_stdout
-  end
-
   it 'diagonal occupied computer' do
     board = Board.new
     game = Game.new(board)
@@ -81,69 +45,5 @@ RSpec.describe Game do
     game = Game.new(board)
     board.cells[1][1].value = "X"
     expect(game.diagnoal_occupied_player?(1,1)).to eq(true)
-  end
-
-  it 'horizontal occupied computer' do
-    board = Board.new
-    game = Game.new(board)
-    board.cells[1][1].value = "O"
-    expect(game.hoz_occupied_computer?(1)).to eq(true)
-  end
-
-  it 'vertical occupied computer' do
-    board = Board.new
-    game = Game.new(board)
-    board.cells[1][1].value = "O"
-    expect(game.ver_occupied_computer?(1)).to eq(true)
-  end
-
-  it 'computer_win methods horizontal' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.computer_win?).to eq(false)
-    game.computer_win_horizontal
-    expect(game.computer_win?).to eq(true)
-    expect(game.computer_win).to output("You lost! :(\n").to_stdout
-  end
-
-  it 'computer_win methods vertical' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.computer_win?).to eq(false)
-    game.computer_win_vertical
-    expect(game.computer_win?).to eq(true)
-    expect(game.computer_win).to output("You lost! :(\n").to_stdout
-  end
-
-  it 'horizontal occupied player' do
-    board = Board.new
-    game = Game.new(board)
-    board.cells[1][1].value = "X"
-    expect(game.hoz_occupied_player?(1)).to eq(true)
-  end
-
-  it 'vertical occupied player' do
-    board = Board.new
-    game = Game.new(board)
-    board.cells[1][1].value = "X"
-    expect(game.ver_occupied_player?(1)).to eq(true)
-  end
-
-  it 'player_win methods horizontal' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.player_win?).to eq(false)
-    game.player_win_horizontal
-    expect(game.player_win?).to eq(true)
-    expect(game.player_win).to output("You won!\n").to_stdout
-  end
-
-  it 'player_win methods vertical' do
-    board = Board.new
-    game = Game.new(board)
-    expect(game.player_win?).to eq(false)
-    game.player_win_vertical
-    expect(game.player_win?).to eq(true)
-    expect(game.player_win).to output("You won!\n").to_stdout
   end
 end
